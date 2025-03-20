2a267bd4cd8f743b240a9e74dc87f4d7426cb540
Clauvio_laptop <2386817103@qq.com>
2025-03-20 19:19:15 +0800

    fx

--- source/simulation.cpp
+++ source/simulation.cpp
@@ -13,6 +13,12 @@ void pause()
     system("pause");
 }
 
+void clear_input()
+{
+    while ('\n' != getchar())
+        ;
+}
+
 void clear()
 {
     COORD coord = {0, 0};
@@ -65,6 +71,110 @@ void draw_a_well(int x_gap, int y_gap, int width, int height, int row, int colum
     }
 }
 
+void draw_a_well1(int origin_x, int origin_y, int width, int height, int row, int column, int flag)
+{
+    int frame_coordinate[2][2];
+    int i = 0;
+
+    {
+        if (flag == 1 || flag == 3)
+        {
+            for (i = 0; i < row; i++)
+            {
+                frame_coordinate[0][0] = origin_x;
+                frame_coordinate[0][1] = origin_y - (int)round((1 + i) * height / row);
+
+                frame_coordinate[1][0] = width + origin_x;
+                frame_coordinate[1][1] = frame_coordinate[0][1];
+
+                line(frame_coordinate[0][0], frame_coordinate[0][1], frame_coordinate[1][0], frame_coordinate[1][1]);
+            }
+        }
+
+        if (flag == 2 || flag == 3)
+        {
+            for (i = 0; i < column; i++)
+            {
+                frame_coordinate[0][0] = origin_x + (int)round((1 + i) * width / column);
+                frame_coordinate[0][1] = origin_y;
+
+                frame_coordinate[1][0] = frame_coordinate[0][0];
+                frame_coordinate[1][1] = origin_y - height;
+
+                line(frame_coordinate[0][0], frame_coordinate[0][1], frame_coordinate[1][0], frame_coordinate[1][1]);
+            }
+        }
+    }
+}
+
+void draw_table_1(double last_point[2], int width, int height, double show_number, int y_gap, double max_num[20], double info[2][500][256], int sim_times, double y_ratio, double *y_scale_ratio)
+{
+    int i = 0, j = 0, RRound, tries, RRound_1, tries_1;
+    double xpercent, xlength, linear_ratio, x_origin, y_origin;
+    double new_point[2];
+    int old_coordinate[2], new_coordinate[2];
+    x_origin = last_point[0];
+    y_origin = last_point[1];
+
+    for (i = 0; i < 2; i++)
+    {
+        last_point[0] = 0;
+        last_point[1] = 0;
+        printf("%lf %lf\n", last_point[0], last_point[1]);
+
+        y_ratio = y_scale_ratio[i] / max_num[i] * height;
+
+        for (j = 0; j < show_number; j++)
+        {
+            xpercent = (j + 1) / (show_number);
+            xlength = xpercent * sim_times;
+
+            new_point[0] = xpercent * width;
+            RRound = (int)floor(xlength) / 256;
+            tries = (int)floor(xlength) % 256;
+            linear_ratio = (xlength) - (256 * RRound + tries);
+
+            if (tries == 255)
+            {
+                RRound_1 = RRound + 1;
+                tries_1 = 0;
+            }
+            else
+            {
+                RRound_1 = RRound;
+                tries_1 = tries + 1;
+            }
+
+            new_point[1] = info[i][RRound][tries] + linear_ratio * (info[i][RRound_1][tries_1] - info[i][RRound][tries]);
+
+            old_coordinate[0] = (int)round(last_point[0] + x_origin);
+            old_coordinate[1] = (int)round(y_convert((int)floor(last_point[1] * y_ratio), height + 2 * y_gap) - y_origin);
+            new_coordinate[0] = (int)round(new_point[0] + x_origin);
+            new_coordinate[1] = (int)round(y_convert((int)floor(new_point[1] * y_ratio), height + 2 * y_gap) - y_origin);
+
+            line(old_coordinate[0], old_coordinate[1], new_coordinate[0], new_coordinate[1]);
+            printf("%d %lf (%d,%d) , (%d,%d)\n", j, y_ratio, old_coordinate[0], old_coordinate[1], new_coordinate[0], new_coordinate[1]);
+
+            last_point[0] = new_point[0];
+            last_point[1] = new_point[1];
+            Sleep(50);
+        }
+    }
+}
+
+void draw_a_coordination(int origin_x, int origin_y, int x_length, int y_length)
+{
+    line(origin_x, origin_y, origin_x + x_length, origin_y);
+    line(origin_x, origin_y, origin_x, origin_y - y_length);
+}
+
+int x_fix(int x, int origin_x, double scale_ratio)
+{
+    int xx = 0;
+    xx = floor(scale_ratio * x + origin_x);
+    return xx;
+}
+
 int main()
 {
 
@@ -114,6 +224,7 @@ int main()
     int new_coordinate[2] = {0};
     int frame_coordinate[100][2] = {0};
     int coordinate_check[4] = {0};
+    int origin_point[2] = {0};
 
     clock_t start, end;
 
@@ -212,19 +323,80 @@ int main()
         setlinecolor(BLACK);
         draw_a_well(x_gap, y_gap, width, height, frame_row, frame_column, 3);
         setlinestyle(PS_SOLID, 3);
-
-        line(x_gap, height + y_gap, width + x_gap, height + y_gap);
-        line(x_gap, height + y_gap, x_gap, y_gap);
-        line(width + x_gap, height + y_gap, width + x_gap, y_gap);
+        draw_a_coordination(x_gap, y_convert(y_gap, height + 2 * y_gap), width, height);
     }
     // 框架
 
     step_sim = sim_times / show_number;
     draw_wait_time = draw_time / show_number * 1000;
 
+    /*
+        for (i = 0; i < 2; i++)
+        {
+            last_point[0] = x_gap, last_point[1] = 0;
+            y_ratio = y_scale_ratio[i] / max_num[i] * height;
+
+            for (j = 0; j < show_number; j++)
+            {
+                xpercent = (j + 1) / (show_number);
+                xlength = xpercent * sim_times;
+
+                new_point[0] = xpercent * width + x_gap;
+                RRound = (int)floor(xlength) / 256;
+                tries = (int)floor(xlength) % 256;
+                linear_ratio = (xlength) - (256 * RRound + tries);
+
+                // printf("%lf %d %lf \n", xpercent * sim_times, 256 * RRound + tries, linear_ratio);
+
+                if (tries == 255)
+                {
+                    RRound_1 = RRound + 1;
+                    tries_1 = 0;
+                }
+                else
+                {
+                    RRound_1 = RRound;
+                    tries_1 = tries + 1;
+                }
+
+                new_point[1] = info[i][RRound][tries] + linear_ratio * (info[i][RRound_1][tries_1] - info[i][RRound][tries]);
+
+                old_coordinate[0] = (int)round(last_point[0]);
+                old_coordinate[1] = (int)round(y_convert((int)floor(last_point[1] * y_ratio), height + 2 * y_gap) - y_gap);
+                new_coordinate[0] = (int)round(new_point[0]);
+                new_coordinate[1] = (int)round(y_convert((int)floor(new_point[1] * y_ratio), height + 2 * y_gap) - y_gap);
+
+                {
+                    coordinate_check[0] = old_coordinate[0];
+                    coordinate_check[1] = old_coordinate[1];
+                    coordinate_check[2] = new_coordinate[0];
+                    coordinate_check[3] = new_coordinate[1];
+                }
+
+                // printf("x percent : %4.3lf RRound : %d tries : %d\n", xpercent, RRound, tries);
+
+                line(old_coordinate[0], old_coordinate[1], new_coordinate[0], new_coordinate[1]);
+                printf("%d %lf (%d,%d) , (%d,%d)\n", j, y_ratio, old_coordinate[0], old_coordinate[1], new_coordinate[0], new_coordinate[1]);
+                //  printf("%d %lf\n", j, show_number);
+
+                last_point[0] = new_point[0];
+                last_point[1] = new_point[1];
+                // Sleep(draw_wait_time);
+            }
+        }
+        clear_input();
+        getchar();
+        cleardevice();
+
+    cleardevice();
+    setlinestyle(PS_SOLID, 1);
+    draw_a_well1(0, y_convert(0, height + 2 * y_gap), width, height, frame_row, frame_column, 3);
+    setlinestyle(PS_SOLID, 3);
+    draw_a_coordination(0, y_convert(0, height + 2 * y_gap), width, height);
+
     for (i = 0; i < 2; i++)
     {
-        last_point[0] = x_gap, last_point[1] = 0;
+        last_point[0] = 0, last_point[1] = 0;
         y_ratio = y_scale_ratio[i] / max_num[i] * height;
 
         for (j = 0; j < show_number; j++)
@@ -232,12 +404,10 @@ int main()
             xpercent = (j + 1) / (show_number);
             xlength = xpercent * sim_times;
 
-            new_point[0] = xpercent * width + x_gap;
+            new_point[0] = xpercent * width;
             RRound = (int)floor(xlength) / 256;
             tries = (int)floor(xlength) % 256;
             linear_ratio = (xlength) - (256 * RRound + tries);
-
-            // printf("%lf %d %lf \n", xpercent * sim_times, 256 * RRound + tries, linear_ratio);
 
             if (tries == 255)
             {
@@ -253,9 +423,9 @@ int main()
             new_point[1] = info[i][RRound][tries] + linear_ratio * (info[i][RRound_1][tries_1] - info[i][RRound][tries]);
 
             old_coordinate[0] = (int)round(last_point[0]);
-            old_coordinate[1] = (int)round(y_convert((int)floor(last_point[1] * y_ratio), height + 2 * y_gap) - y_gap);
+            old_coordinate[1] = (int)round(y_convert((int)floor(last_point[1] * y_ratio), height + 2 * y_gap));
             new_coordinate[0] = (int)round(new_point[0]);
-            new_coordinate[1] = (int)round(y_convert((int)floor(new_point[1] * y_ratio), height + 2 * y_gap) - y_gap);
+            new_coordinate[1] = (int)round(y_convert((int)floor(new_point[1] * y_ratio), height + 2 * y_gap));
 
             {
                 coordinate_check[0] = old_coordinate[0];
@@ -264,17 +434,25 @@ int main()
                 coordinate_check[3] = new_coordinate[1];
             }
 
-            // printf("x percent : %4.3lf RRound : %d tries : %d\n", xpercent, RRound, tries);
-
             line(old_coordinate[0], old_coordinate[1], new_coordinate[0], new_coordinate[1]);
             printf("%d %lf (%d,%d) , (%d,%d)\n", j, y_ratio, old_coordinate[0], old_coordinate[1], new_coordinate[0], new_coordinate[1]);
-            //  printf("%d %lf\n", j, show_number);
 
             last_point[0] = new_point[0];
             last_point[1] = new_point[1];
-            Sleep(draw_wait_time);
+            // Sleep(draw_wait_time);
         }
     }
+        */
+
+    cleardevice();
+    setlinestyle(PS_SOLID, 1);
+    draw_a_well1(0, y_convert(0, height + 2 * y_gap), width, height, frame_row, frame_column, 3);
+    setlinestyle(PS_SOLID, 3);
+    draw_a_coordination(0, y_convert(0, height + 2 * y_gap), width, height);
+
+    last_point[0] = 500, last_point[1] = 500;
+
+    draw_table_1(last_point, width, height, show_number, y_gap, max_num, info, sim_times, y_ratio, y_scale_ratio);
 
     {
         printf("Sim_times : %d\n", sim_times);
